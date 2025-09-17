062988964a546da75fcb65df2031495d12938a88
Dhyey Jariwala <90595404+DhyeyJariwala99@users.noreply.github.com>
2025-09-17 17:32:25 -0400

    Update page.tsx

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -4,7 +4,7 @@ export default function Labs() {
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
-      <p>Salman Shariff - Full Stack Web Development</p>
+      <p>Dhyey Jariwala - Full Stack Web Development</p>
       <ul>
         <li>
           <Link href="/Labs/Lab1" id="wd-lab1-link">
@@ -34,4 +34,5 @@ export default function Labs() {
       </ul>
     </div>
   );
+
 }
